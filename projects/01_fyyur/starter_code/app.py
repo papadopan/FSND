744181e872b5ffbab928e720185eaba1c8732b23
papadopan <antonios.papadopan@gmail.com>
2020-02-24 21:17:22 +0100

    fetch venues grouped by city name and state

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -135,27 +135,17 @@ def index():
 def venues():
     # TODO: replace with real venues data.
     #       num_shows should be aggregated based on number of upcoming shows per venue.
-    data = [{
-        "city": "San Francisco",
-        "state": "CA",
-        "venues": [{
-            "id": 1,
-            "name": "The Musical Hop",
-            "num_upcoming_shows": 0,
-        }, {
-            "id": 3,
-            "name": "Park Square Live Music & Coffee",
-            "num_upcoming_shows": 1,
-        }]
-    }, {
-        "city": "New York",
-        "state": "NY",
-        "venues": [{
-            "id": 2,
-            "name": "The Dueling Pianos Bar",
-            "num_upcoming_shows": 0,
-        }]
-    }]
+    records = db.session.query(func.count(Venue.city), Venue.city, Venue.state).group_by(
+        Venue.city, Venue.state).all()
+
+    data = []
+    for i in records:
+        record = {
+            "city": i.city,
+            "state": i.state,
+            "venues": Venue.query.filter_by(city=i.city, state=i.state).all()
+        }
+        data.append(record)
 
     return render_template('pages/venues.html', areas=data)
 
